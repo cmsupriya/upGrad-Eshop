ebb044a3528ef1434239df40ba547dd91d726726
Supriya Sahoo <cm.supriyasahoo@gmail.com>
2024-08-02 16:29:35 +0530

    Routing changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from './components/SignUp';
 import Navbar from './components/Navbar';
 import Login from './components/Login';
 
 function App() {
-  const router = RouterProvider([
-    {
-      path: "/login",
-      element: <Login/>
-    },
-    {
-      path: "/signUp",
-      element: <SignUp/>
-    },
-  ])
+
 
   return (
-    <>
-    <Navbar/>
-      <RouterProvider router={router}/>
-    </>
+
+      <Router>
+      <Navbar/>
+      <Routes>
+        
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+
+      </Routes>
+      </Router>
+
+
   )
 }
 
